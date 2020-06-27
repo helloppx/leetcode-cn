d285dba7cf8b359a83a184340f12eac8242876cf
helloppx <helloppx@gmail.com>
2020-06-27 11:44:21 +0800

    Update Print FooBar Alternately.java

--- Print FooBar Alternately.java
+++ Print FooBar Alternately.java
@@ -27,3 +27,42 @@ class FooBar {
         }
     }
 }
+
+
+
+class FooBar {
+    private int n;
+
+    private volatile boolean foo = false;
+    private volatile boolean bar = true;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            while(foo) {
+                Thread.yield();
+            }
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            foo = true;
+            bar = false;
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            while(bar) {
+                Thread.yield();
+            }
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            bar = true;
+            foo = false;
+        }
+    }
+}
