0f962267afb80ac49708613bc40e965f05160ed4
helloppx <helloppx@gmail.com>
2020-09-29 15:30:40 +0800

    Update Print Zero Even Odd.java

--- Print Zero Even Odd.java
+++ Print Zero Even Odd.java
@@ -44,3 +44,54 @@ class ZeroEvenOdd {
         }
     }
 }
+
+
+
+
+
+class ZeroEvenOdd {
+    private int n;
+    private volatile int state = 0;
+    private volatile boolean flag = false;
+    
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 0; i < n; i++){
+            while(state != 0){
+                Thread.yield();
+            }
+            printNumber.accept(0);
+            if(flag){
+                state = 1;
+            }else{
+                state = 2;
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 2; i <= n; i+=2){
+            while(state != 1){
+                Thread.yield();
+            }
+            printNumber.accept(i);
+            state = 0;
+            flag = false;
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for(int i = 1; i <= n; i+=2){
+            while(state != 2){
+                Thread.yield();
+            }
+            printNumber.accept(i);
+            state = 0;
+            flag = true;
+        }
+    }
+}
